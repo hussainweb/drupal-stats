af7e5ffa1dec83cbef6f1e839377985309414604
hw <hussainweb@gmail.com>
2016-02-02 15:56:40 +0530

    Change application name and update dependencies

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Drupal Statistics Collector</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Drupal Statistics Collector</div>
             </div>
         </div>
     </body>
