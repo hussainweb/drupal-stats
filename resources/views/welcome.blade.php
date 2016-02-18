f2e12e6e8d1491718d2c0b9411c645ba900b6533
hw <hussainweb@gmail.com>
2016-02-18 18:50:59 +0530

    Add descriptions on homepage and compress assets for production

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
                 <img src="{{ asset('images/01-module-downloads.png') }}" alt="Module Downloads Bubble">
             </a>
             <h3>Module Downloads Bubble</h3>
-            <p></p>
+            <p>The top 200 modules from drupal.org arranged by downloads and category.</p>
             <a href="{{ url('viz/module-downloads') }}" class="btn btn-info">View</a>
         </div>
         <div class="col-sm-4">
@@ -18,7 +18,7 @@
                 <img src="{{ asset('images/02-project-downloads.png') }}" alt="Project Downloads Bubble">
             </a>
             <h3>Project Downloads Bubble</h3>
-            <p></p>
+            <p>The top 200 projects from drupal.org arranged by downloads and project type.</p>
             <a href="{{ url('viz/project-downloads') }}" class="btn btn-info">View</a>
         </div>
         <div class="col-sm-4">
@@ -26,15 +26,17 @@
                 <img src="{{ asset('images/03-projects-growth.png') }}" alt="Project Growth">
             </a>
             <h3>Project Growth</h3>
-            <p></p>
+            <p>The growth of each project type over the years.</p>
             <a href="{{ url('viz/projects-growth') }}" class="btn btn-info">View</a>
         </div>
+    </div>
+    <div class="row">
         <div class="col-sm-4">
             <a href="{{ url('viz/issues') }}">
                 <img src="{{ asset('images/04-issues-breakdown.png') }}" alt="Issues Breakdown">
             </a>
             <h3>Issues Breakdown</h3>
-            <p></p>
+            <p>Breakdown of issues for all projects or for a specific project.</p>
             <a href="{{ url('viz/issues') }}" class="btn btn-info">View</a>
         </div>
         <div class="col-sm-4">
@@ -42,7 +44,7 @@
                 <img src="{{ asset('images/05-user-countries.png') }}" alt="User Countries">
             </a>
             <h3>User Countries</h3>
-            <p></p>
+            <p>User registrations on drupal.org per each country in the world.</p>
             <a href="{{ url('viz/user-countries') }}" class="btn btn-info">View</a>
         </div>
         <div class="col-sm-4">
@@ -50,15 +52,17 @@
                 <img src="{{ asset('images/06-user-languages.png') }}" alt="User Languages">
             </a>
             <h3>User Languages</h3>
-            <p></p>
+            <p>Languages spoken by drupal.org users</p>
             <a href="{{ url('viz/user-languages') }}" class="btn btn-info">View</a>
         </div>
+    </div>
+    <div class="row">
         <div class="col-sm-4">
             <a href="{{ url('viz/user-expertise') }}">
                 <img src="{{ asset('images/07-user-expertise.png') }}" alt="User Expertise">
             </a>
             <h3>User Expertise</h3>
-            <p></p>
+            <p>Areas of expertise of drupal.org users</p>
             <a href="{{ url('viz/user-expertise') }}" class="btn btn-info">View</a>
         </div>
         <div class="col-sm-4">
@@ -66,7 +70,7 @@
                 <img src="{{ asset('images/08-tests-status.png') }}" alt="CI Jobs Status">
             </a>
             <h3>CI Jobs Status</h3>
-            <p></p>
+            <p>Status of CI jobs running on each branch</p>
             <a href="{{ url('viz/ci-jobs') }}" class="btn btn-info">View</a>
         </div>
         <div class="col-sm-4">
@@ -74,7 +78,7 @@
                 <img src="{{ asset('images/09-tests-reasons.png') }}" alt="CI Jobs Reasons">
             </a>
             <h3>CI Jobs Reasons</h3>
-            <p></p>
+            <p>Reasons for each CI job run</p>
             <a href="{{ url('viz/ci-jobs-reasons') }}" class="btn btn-info">View</a>
         </div>
     </div>
