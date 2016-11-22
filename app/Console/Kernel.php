8e5fcfe4b5c8f2e95bddfa8d7b73698f8e652c57
hw <hussainweb@gmail.com>
2016-11-22 11:55:39 +0530

    Remove leftover files from Laravel 5.2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,7 +20,6 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\Inspire::class,
         GetNewUsers::class,
         GetNodes::class,
         GetTerms::class,
